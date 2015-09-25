67d9864bd7cc0bbfe5d9c65390b4f714d2982997
iCodeart <servicios@icodeart.com>
2015-09-24 21:26:55 -0300

    Eliminar eventos

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $servidor='localhost';									    //
 $usuario='root';											//
-$pass='';											        // Nos conectamos a
-$bd='calendario';											// la base de datos
+$pass='';								        // Nos conectamos a
+$bd='eventos';											// la base de datos
 															//
 $conexion = new mysqli($servidor, $usuario, $pass, $bd);	//
 $conexion->set_charset('utf8'); // Los datos vendran en formato utf-8
@@ -11,6 +11,6 @@ if ($conexion->connect_errno) {                                            //
 	echo "Error al conectar la base de datos {$conexion->connect_errno}";  // Mostramos algun posble error
 }                                                                          //
 
-$base_url="http://localhost/calendario2/"; // Url donde estara el proyecto
+$base_url="http://localhost/calendario/"; // Url donde estara el proyecto, debe terminar con un "/" al final
 
 ?>
