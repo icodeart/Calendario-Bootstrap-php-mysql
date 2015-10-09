d095845d5cb549b50daf96723aa3d3a234a10660
iCodeart <servicios@icodeart.com>
2015-10-09 18:18:48 -0200

    Mejora en el codigo

--- config.php
+++ config.php
@@ -1,16 +1,23 @@
 <?php
-$servidor='localhost';									    //
-$usuario='root';											//
-$pass='';								        // Nos conectamos a
-$bd='eventos';											// la base de datos
-															//
-$conexion = new mysqli($servidor, $usuario, $pass, $bd);	//
-$conexion->set_charset('utf8'); // Los datos vendran en formato utf-8
 
-if ($conexion->connect_errno) {                                            //
-	echo "Error al conectar la base de datos {$conexion->connect_errno}";  // Mostramos algun posble error
-}                                                                          //
+// Datos de conexion a la base de datos
+$servidor='localhost';
+$usuario='root';
+$pass='';
+$bd='eventos';
 
-$base_url="http://localhost/calendario/"; // Url donde estara el proyecto, debe terminar con un "/" al final
+// Nos conectamos a la base de datos
+$conexion = new mysqli($servidor, $usuario, $pass, $bd);	
+
+// Definimos que nuestros datos vengan en utf8
+$conexion->set_charset('utf8');
+
+// verificamos si hubo algun error y lo mostramos
+if ($conexion->connect_errno) {
+	echo "Error al conectar la base de datos {$conexion->connect_errno}";
+}
+
+// Url donde estara el proyecto, debe terminar con un "/" al final
+$base_url="http://localhost/calendario/";
 
 ?>
